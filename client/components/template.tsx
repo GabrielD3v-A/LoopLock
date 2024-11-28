412cada683607178b22ae9c3ab80ca6c1de17b10
GabrielD3v-A <gabrielskt890@gmail.com>
2024-11-28 02:28:44 -0300

    feat: Added HomePage, new components
    In one of the components, I created the ButtonComponent that will be reused throughout the project with different functions

---
+++ client/components/template.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StatusBar, View } from 'react-native';
+
+interface TemplateProps {
+    children: React.ReactNode;
+  }
+
+const Template = ({ children }: TemplateProps) => {
+    return (
+      <View className="flex-1  w-full max-w-full max-h-screen mx-auto p-0 bg-white">
+         <StatusBar barStyle="light-content" backgroundColor="#03045E" animated={true}/>
+        {children} 
+      </View>
+    );
+  };
+
+export default Template;
