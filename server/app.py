6bf7c42fcd53321b813b7c1ec786c56c195521b7
Lucskin <luquis302@gmail.com>
2024-11-30 18:18:14 -0300

    Sua mensagem de commit

--- server/app.py
+++ server/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify # type: ignore
 from routes.main_routes import main_routes
+from flask import render_template
 
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
 app.register_blueprint(main_routes)
 @app.route('/')
