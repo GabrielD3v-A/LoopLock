613f2df5a6bc3709892a7a8cd5eee7614c3c1bb2
GabrielD3v-A <gabrielskt890@gmail.com>
2025-05-16 13:43:08 -0300

    fix: adjust main for railway

--- server/app.py
+++ server/app.py
@@ -77,5 +77,5 @@ def ping():
         return {"message": "Erro na conexão", "error": str(e)}, 500
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
