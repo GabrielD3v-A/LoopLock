b44a981a5fe2f426312c0fdccf643e46bdd2246d
GabrielD3v-A <gabrielskt890@gmail.com>
2025-05-29 12:33:56 -0300

    feat: add searchBar component and add header component

--- client/components/header.tsx
+++ client/components/header.tsx
@@ -7,11 +7,11 @@ const Header = () => {
     const [searchQuery, setSearchQuery] = React.useState('');
 
     return (
-        <View style={{ height: 80 }}  className='fixed w-full top-0 left-0 bg-lp-blue flex flex-row items-center justify-between px-4  z-10 py-10 rounded-b-3xl'>
-            <View className='flex items-start justify-end w-1/2'>
+        <View style={{ height: 80 }}  className='fixed w-full top-0 left-0 bg-lp-blue flex flex-row items-center justify-between  px-4  z-10 py-10 rounded-b-3xl'>
+            <View className='flex items-start justify-end w-3/12'>
                 <Logo />
             </View>
-            <View className='flex flex-row items-center justify-between w-1/2 gap-x-4'>
+            <View className='flex flex-row items-center justify-between w-9/12 gap-x-8'>
                 <View className='w-40 h-5'><SearchBar /></View>
                 <Image source={require('@/assets/images/icons/user-icon.png')} style={{ width: 40, height: 40 }} />
             </View>
--- client/components/searchBar.tsx
+++ client/components/searchBar.tsx
@@ -3,9 +3,9 @@ import { StyleSheet, TextInput, View, Image } from 'react-native';
 
 const SearchBar = () => {
     return (
-        <View className='flex flex-row items-center justify-between w-full  px-2 py-1 bg-[#CBCCF5] rounded-full  border-white box-border' style={{ height: 35, borderWidth: 1, borderColor: '#fff' }}>
+        <View className='flex flex-row items-center justify-between w-full px-2 py-1 bg-[#CBCCF5] rounded-full  border-white box-border' style={{ height: 40, borderWidth: 1, borderColor: '#fff' }}>
             <Image source={require('../assets/images/icons/search-icon.png')} style={{ width: 22, height: 22 }} />
-            <TextInput placeholder="" className='h-full w-full bg-transparent text-white' placeholderTextColor={'#fff'} />
+            <TextInput placeholder="" className='h-full w-full bg-transparent text-white px-2' placeholderTextColor={'#fff'}  multiline={true} maxLength={100}/>
         </View>
     );
 }
