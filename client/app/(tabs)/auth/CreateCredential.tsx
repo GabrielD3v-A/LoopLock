52634bea575824fb10f88ffd9e1e4f4d69836027
GabrielD3v-A <gabrielskt890@gmail.com>
2025-06-16 00:35:54 -0300

    feat: added Create and List Credential's user and added create-credential page

--- client/app/(tabs)/auth/CreateCredential.tsx
+++ client/app/(tabs)/auth/CreateCredential.tsx
@@ -1,10 +1,113 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import SafeProvider, { useSafe } from '@/app/context/SafeContext'
+import { Pressable, TextInput } from 'react-native-gesture-handler';
 
-export default function CreateCredential() {
+export default function CreateCredentialWrapper() {
   return (
-    <View>
-      <Text>create-password</Text>
+    <SafeProvider>
+      <CreateCredential />
+    </SafeProvider>
+  );
+}
+
+function CreateCredential() {
+  const { onCreateSafe } = useSafe();
+
+  const [place, setPlace] = React.useState('');
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [domain, setDomain] = React.useState('');
+
+  const handleCreateCredential = () => {
+    
+  };
+
+  const handleDiscardCredential = () => {
+    
+  };
+
+  return (
+    <View className='flex-1 items-center justify-start bg-white px-8 gap-y-1'>
+      <Text className='text-3xl font-normal text-lp-blue mt-10 mb-5' style={{ fontFamily: 'Montserrat-Regular' }}>Manage Password</Text>
+
+      <View className='w-full flex flex-col items-center justify-start gap-y-2 h-auto bg-lp-lilas-3 rounded-2xl px-8 py-5'>
+        
+        <View className='flex flex-col items-start gap-y-1 w-full mt-6'>
+          <Text className='text-lp-blue text-sm text-left' style={{ fontFamily: 'Montserrat-Medium' }}>Nome da plataforma</Text>
+          <View className='w-full flex flex-row items-center gap-x-1 bg-lp-lilas rounded-lg px-3 h-14'>
+            <Image source={require('@/assets/images/credentials/template.png')} className='w-7 h-7'/>
+            <TextInput 
+              className='w-11/12 h-full text-lp-blue text-sm pt-4' 
+              placeholder='Selecionar plataforma' 
+              placeholderTextColor='#03045E' 
+              style={{ fontFamily:'Montserrat-Regular' }}
+              onChangeText={setPlace}
+              numberOfLines={1}
+              maxLength={30}
+              >  
+            </TextInput>
+          </View>
+        </View>
+
+        <View className='flex flex-col items-start gap-y-1 w-full mt-6'>
+          <Text className='text-lp-blue text-sm text-left' style={{ fontFamily: 'Montserrat-Medium' }}>Dominio</Text>
+          <TextInput 
+            className='w-full h-14 bg-lp-lilas rounded-lg text-lp-blue px-3 text-sm' 
+            placeholder='Adicione seu username' 
+            placeholderTextColor='#03045E' 
+            style={{ fontFamily:'Montserrat-Regular' }}
+            onChangeText={setDomain}
+            numberOfLines={1}
+            >
+          </TextInput>
+        </View>
+
+        <View className='flex flex-col items-start gap-y-1 w-full mt-6'>
+          <Text className='text-lp-blue text-sm text-left' style={{ fontFamily: 'Montserrat-Medium' }}>Username ou e-mail</Text>
+          <TextInput 
+            className='w-full h-14 bg-lp-lilas rounded-lg text-lp-blue px-3 text-sm' 
+            placeholder='Adicione seu username' 
+            placeholderTextColor='#03045E' 
+            style={{ fontFamily:'Montserrat-Regular' }}
+            onChangeText={setUsername}
+            numberOfLines={1}
+            autoComplete={'email'}
+            maxLength={30}
+            >
+          </TextInput>
+        </View>
+
+        <View className='flex flex-col items-start gap-y-1 w-full mt-6'>
+          <Text className='text-lp-blue text-sm text-left' style={{ fontFamily: 'Montserrat-Medium' }}>Senha</Text>
+          <TextInput 
+            className='w-full h-14 bg-lp-lilas rounded-lg text-lp-blue px-3 text-sm ' 
+            placeholder='sua senha'
+            placeholderTextColor='#03045E'  
+            style={{ fontFamily:'Montserrat-Regular' }}
+            onChangeText={setPassword}
+            numberOfLines={1}
+            secureTextEntry={true}
+            >
+          </TextInput>
+          <Text className='text-[0.45rem] text-lp-blue font-regular' style={{ fontFamily:'Montserrat-Regular' }}>Certifique-se que sua senha é válida na plataforma que está editando.</Text>
+        </View>
+
+
+
+        <View className='w-full flex flex-row items-center justify-center gap-x-8 mt-10'>
+          <Pressable onPress={handleDiscardCredential}>
+            <Text className='text-lp-blue text-base underline' style={{ fontFamily: 'Montserrat-Medium' }}>Descartar</Text>
+          </Pressable>
+          <View className='w-24'>
+            <TouchableOpacity onPress={handleCreateCredential}>
+              <View className='flex flex-row items-center justify-center w-full bg-lp-lightblue py-2 rounded-full'>
+                <Text className='text-white text-base font-semibold' style={{ fontFamily: 'Montserrat-Medium' }}>Salvar</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
-  )
+  );
 }
--- client/app/context/SafeContext.tsx
+++ client/app/context/SafeContext.tsx
@@ -12,21 +12,21 @@ interface safeProps {
     safeState: SafeState;
     loading: boolean;
     onListSafe: () => void;
-    onCreateSafe: () => void;
+    onCreateSafe: (name: string, username: string, password: string, domain: string) => void;
     onEditSafe: () => void;
     onDeleteSafe: () => void;
 }
 
 const safeContext = createContext<safeProps | undefined>(undefined);
 
-export const safe = () => {
+export const useSafe = () => {
     const context = useContext(safeContext);
     if (!context) throw new Error('useAuth must be used within an AuthProvider');
     return context;
 }
 let token_key: string | null = 'token';
 let simetric_key: string | null = 'simetric_key';
-
+let API_URL = 'https://looplock.onrender.com/credential';
 export const SafeProvider = ({children}: {children: ReactNode}) => {
 
     const [safeState, setSafeState] = useState<SafeState>({
@@ -46,7 +46,7 @@ export const SafeProvider = ({children}: {children: ReactNode}) => {
                 simetric_key = await SecureStore.getItemAsync('simetric_key');
                 if (token_key && simetric_key) {
                     axios.defaults.headers.common['Authorization'] = `Bearer ${token_key}`;
-                    setSafeState({ token: token_key, authenticated: true, simetric_key });
+                    setSafeState({ token: token_key, authenticated: true, simetric_key: simetric_key });
                 } else {
                     setSafeState({ token: null, authenticated: false, simetric_key: null });
                 }
@@ -59,8 +59,34 @@ export const SafeProvider = ({children}: {children: ReactNode}) => {
         checkToken();
     }, []);
 
-    const listSafe = () => {};
-    const createSafe = () => {};
+    const listSafe = async () => {
+        try {
+            const result = await axios.get(`${API_URL}/list`);
+            return { data: result.data };
+        } catch (err: any) {
+            return { error: true, message: err?.response?.data?.message || 'Erro ao listar cofre' };
+        }
+    };
+    const createSafe = async (name: string, username: string, password: string, domain: string) => {
+        try {
+            const result = await axios.post(`${API_URL}/create`, {
+                "name": name,
+                "username": username,
+                "password": password,
+                "domain": domain,
+                "simetric_key": safeState.simetric_key
+            });
+
+            if (result.data?.error) {
+                alert(result.data.msg);
+                return { error: true, message: result.data.msg };
+            } else {
+                return result;
+            }
+        } catch (error: any) {
+            return { error: true, message: error?.response?.data?.message || 'Erro na requisição' };
+        }
+    };
     const editSafe = () => {};
     const deleteSafe = () => {};
 
